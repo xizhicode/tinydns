eb4449a26ae405ca0e92d71d89ea4ed8b8c85fd9
zhaoheng12 <1046377807@qq.com>
2020-02-17 19:47:02 +0800

    dns 服务

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 # tinydns
 
 基于gevent的轻量dns服务
+
+* 依赖的包 gevent,dnslib  python2:ConfigParser python3:configparser
+
+* 方法1： pip install tinydns == 0.0.7
+
+* 方法二： git clone https://github.com/zhaoheng12/tinydns.git
+
+* 在 /etc 下 配置 tinydns.conf
+        [gevent_dns]
+        AF_INET = 2
+        SOCK_DGRAM = 2
+        port = 53
+* 运行命令：sudo tinydns -c  /etc/tinydns.conf
+
+
+* 测试： dig @127.0.0.1 google.com
---
+++ setup.py
@@ -0,0 +1,32 @@
+import shutil
+from pkg_resources import Requirement, resource_filename
+try:
+    from setuptools import setup,find_packages
+except ImportError:
+    from distutils.core import setup,find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="tinydns",
+    packages=find_packages(),
+    version='0.0.7',
+    description='this project is a tinydns automaton implementation by python',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='git@github.com:xizhicode/tinydns.git',
+    author='zhaohengping',
+    author_email='18438697706@163.com',
+    data_files = [('/etc', ['tinydns.conf'])],
+    install_requires=["gevent","dnslib"],
+    entry_points={
+                  'console_scripts': [
+                      'tinydns=tinydns.__init__:main',
+                  ],
+              },
+    package_data={
+            '': ['*.rst'],
+        }
+
+)
